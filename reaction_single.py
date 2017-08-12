bef2558ab3e2e377a42f24ebadc89f5f47cb5cf7
Jonathan <jonathan@ihle.in>
2017-08-12 18:21:27 +0800

    Added comments

--- reaction_single.py
+++ reaction_single.py
@@ -1,14 +1,19 @@
 from microbit import *
 import random
+#Generate a random time between 2 and 10 seconds
 wait_time = random.randint(2000, 10000)
+#Sleep for the randomly generated time
 display.show(Image.ASLEEP)
 sleep(wait_time)
+#This if statement checks if the user pressed the button early
 if button_a.get_presses():
     display.show(Image.NO)
 else:
+    #notify the user that they can press the button
     display.show(Image.TARGET)
     waiting = True
     reaction_time = 0
+    #Keep counting up until the user presses the button
     while waiting:
         if button_a.is_pressed():
             waiting = False
@@ -17,4 +22,5 @@ else:
     display.show(Image.YES)
     sleep(1000)
     while True:
+        #Show the reaction time. This is not terribly accurate
         display.scroll(str(reaction_time))
